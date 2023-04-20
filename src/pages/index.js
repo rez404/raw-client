e7875633360e22b6e3ae2f2a3410b96134ed4fed
admin <support@bitchain.llc>
2023-04-20 06:25:15 +0300

    favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,24 @@ export default function Home() {
           content="Rawblock is a project that adapts various Web2 systems to the Web3 infrastructure."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Header />
       <HeroSection />
